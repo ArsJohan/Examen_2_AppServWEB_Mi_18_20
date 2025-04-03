1f997b4a3d068b16c5127170a472724aefcfbb15
ArsJohan <133719384+ArsJohan@users.noreply.github.com>
2025-04-03 02:01:36 -0500

    Servicio Imagenes

---
+++ Examen2/Controllers/UploadFilesController.cs
@@ -0,0 +1,44 @@
+﻿using Examen2.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Examen2.Controllers
+{
+    [RoutePrefix("api/UploadFiles")]
+    public class UploadFilesController : ApiController
+    {
+        [HttpPost]
+        public async Task<HttpResponseMessage> CargarArchivo(HttpRequestMessage request, string Datos, string Proceso)
+        {
+            clsUpload upload = new clsUpload();
+            upload.Datos = Datos;
+            upload.Proceso = Proceso;
+            upload.request = request;
+            return await upload.GrabarArchivo(false);
+        }
+
+        [HttpPut]
+        public async Task<HttpResponseMessage> Actualizar(HttpRequestMessage request, string Datos, string Proceso)
+        {
+            clsUpload upload = new clsUpload();
+            upload.Datos = Datos;
+            upload.Proceso = Proceso;
+            upload.request = request;
+            return await upload.GrabarArchivo(true);
+        }
+        [HttpDelete]
+        public HttpResponseMessage EliminarArchivo(HttpRequestMessage request, string NombreArchivo, string Proceso)
+        {
+            clsUpload upload = new clsUpload();
+            upload.Proceso = Proceso;
+            upload.request = request;
+            return upload.EliminarArchivo(NombreArchivo);
+        }
+        
+    }
+}
